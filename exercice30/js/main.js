c9650c2b569941b21ba4807acc6e11ee1bf4633d
Mgoumane <goumane.mahmoudou@gmail.com>
2021-01-12 16:41:44 +0100

    ex30 ok

---
+++ exercice30/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("input").keyup(function () {
+        if ($("input").val().length > 5) {
+            $("input").removeClass("is-invalid")
+            $("input").addClass("is-valid")
+        }else{
+            $("input").addClass("is-invalid")
+        }
+    });
+});
